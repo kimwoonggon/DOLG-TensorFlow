c592a90cdf59a2fde1b57f7dd5d18e4ff6148fd1
김웅곤 <60002940+kimwoonggon@users.noreply.github.com>
2022-02-14 20:52:55 +0900

    Update LocalBranch.py

--- layers/LocalBranch.py
+++ layers/LocalBranch.py
@@ -1,62 +1,49 @@
+"""
+Created on Tue Oct 26 02:33:39 2021
+@author: innat
+"""
 import tensorflow as tf 
 from tensorflow import keras 
 from tensorflow.keras import layers 
 
 # Multi-Atrous Branch
 class MultiAtrous(keras.Model):
-    def __init__(self, dilation_rates=[6, 12, 18], upsampling=1, 
+    def __init__(self, dilation_rates=[3, 6, 12], upsampling=1, 
                  kernel_size=3, padding="same",  **kwargs):
         super(MultiAtrous, self).__init__(name='MultiAtrous', **kwargs)
         self.dilation_rates = dilation_rates
         self.kernel_size = kernel_size 
         self.upsampling = upsampling
         self.padding = padding
-
-        self.dilation_convs0 = layers.Conv2D(
-                                    filters       = 512, 
-                                    kernel_size   = 3,  
+        # Dilated Convolutions                     
+        self.dilated_convs = [
+                                layers.Conv2D(
+                                    filters       = int(1024 / 4), 
+                                    kernel_size   = rate,  
                                     padding       = self.padding, 
-                                    #dilation_rate = 6
-                                )
-        self.dilation_convs1 = layers.Conv2D(
-                                    filters       = 512, 
-                                    kernel_size   = 6,  
-                                    padding       = self.padding, 
-                                    #dilation_rate = 12
-                                )
-        self.dilation_convs2 = layers.Conv2D(
-                                    filters       = 512, 
-                                    kernel_size   = 9,  
-                                    padding       = self.padding, 
-                                    #dilation_rate = 18
-                                )
-        self.conv1 = layers.Conv2D(
-            filters=512,
-            kernel_size=1,
-            padding='same')
+                                    #dilation_rate = rate
+                                ) for rate in self.dilation_rates
+                             ]
+        
         # Global Average Pooling Branch 
         self.gap_branch = keras.Sequential(
             [
                 layers.GlobalAveragePooling2D(keepdims=True),
-                layers.Conv2D(512, kernel_size=1),
+                layers.Conv2D(int(1024 / 2), kernel_size=1),
                 layers.Activation('relu'),
-                #layers.UpSampling2D(size=self.upsampling, interpolation="bilinear")
+                layers.UpSampling2D(size=self.upsampling, interpolation="bilinear")
             ] , name='gap_branch'
         )
         
     def call(self, inputs, training=None, **kwargs):
         local_feature = []
 
-        #for dilated_conv in self.dilated_convs:
-        x0 = self.dilation_convs0(inputs) 
-        #x = self.gap_branch(x)
-        x1 = self.dilation_convs1(inputs)
-        x2 = self.dilation_convs2(inputs)
-        x = tf.concat([x0,x1,x2],axis=-1)
-        x = self.conv1(x)
-        x = tf.nn.relu(x)
+        for dilated_conv in self.dilated_convs:
+            x = dilated_conv(inputs) 
+            x = self.gap_branch(x)
+            local_feature.append(x)
             
-        return x
+        return tf.concat(local_feature, axis=-1)
 
     def get_config(self):
         config = {
@@ -78,7 +65,7 @@ class DOLGLocalBranch(keras.Model):
         self.conv2 = layers.Conv2D(1024, kernel_size=1, use_bias=False)
         self.conv3 = layers.Conv2D(1024, kernel_size=1)
         self.bn = layers.BatchNormalization()
-        self.glpool = layers.GlobalAveragePooling2D(keepdims=True)
+
     def call(self, inputs, training=None, **kwargs):
         # Local Branach + Normalization / Conv-Bn Module 
         local_feat = self.multi_atrous(inputs)
@@ -88,9 +75,9 @@ class DOLGLocalBranch(keras.Model):
         # Self-Attention
         local_feat = self.conv2(local_feat)
         local_feat = self.bn(local_feat)
-        local_feat = self.glpool(local_feat)
+
         # l-2 norms
-        norm_local_feat = tf.math.l2_normalize(local_feat, axis=-1)
+        norm_local_feat = tf.math.l2_normalize(local_feat)
 
         # softplus activations
         attn_map = tf.nn.relu(local_feat)
@@ -98,4 +85,4 @@ class DOLGLocalBranch(keras.Model):
         attn_map = keras.activations.softplus(attn_map) 
 
         # Output of the Local-Branch 
-        return  norm_local_feat * attn_map
+        return  norm_local_feat * attn_map 
